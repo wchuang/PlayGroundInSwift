f01178bf813509d2bb02924c078143bc8220db8b
Frank Huang <ijamhh@gmail.com>
2018-06-20 01:41:46 +0800

    Record it

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -159,9 +159,111 @@ var points = [p1, p2, p3]
 points.sorted(by: ==)
 points.sorted(by: <)
 
+// Class
 
+class NamedShape {
+    
+    var numberOfSides: Int = 0
+    var name: String
+    
+    init(name: String) {
+        self.name = name
+    }
+    
+    func simpleDescription() -> String {
+        return "A shape with \(numberOfSides) sides."
+    }
+}
 
+class EquilateralTriangle: NamedShape {
+    
+    var sideLength: Double = 0.0
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 3
+    }
+    
+    var perimeter: Double {
+        get {
+            print("get")
+            return 3.0 * sideLength
+        }
+        set(aValue) {
+            print("set")
+            sideLength = aValue / 3.0
+        }
+        /*
+        set {
+            sideLength = newValue / 3.0
+        }
+         */
+    }
+    
+    override func simpleDescription() -> String {
+        return "An equilateral triangle with sides of length \(sideLength)"
+    }
+}
 
+var triangle = EquilateralTriangle(sideLength: 3.1, name: "a triangle")
+print(triangle.perimeter)
+triangle.perimeter = 3
+print(triangle.perimeter)
 
+class Square: NamedShape {
+    
+    var sideLength: Double
+    
+    init(sideLength: Double, name: String) {
+        self.sideLength = sideLength
+        super.init(name: name)
+        numberOfSides = 4
+    }
+    
+    func area() -> Double {
+        return sideLength * sideLength
+    }
+    
+    override func simpleDescription() -> String {
+        return "A square with sides of length \(sideLength)"
+    }
+}
 
+let test = Square(sideLength: 5.2, name: "my test square")
+test.area()
+test.simpleDescription()
+
+class TriangleAndSquare {
+    
+    var triangle: EquilateralTriangle {
+        willSet {
+            square.sideLength = newValue.sideLength
+        }
+    }
+    
+    var square: Square {
+        willSet {
+            triangle.sideLength = newValue.sideLength
+        }
+    }
+    
+    init(size: Double, name: String) {
+        square = Square(sideLength: size, name: name)
+        triangle = EquilateralTriangle(sideLength: size, name: name)
+    }
+}
+
+var triangleAndSquare = TriangleAndSquare(size: 10, name: "another test shape")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+
+triangleAndSquare.square = Square(sideLength: 50, name: "larger square")
+print(triangleAndSquare.square.sideLength)
+print(triangleAndSquare.triangle.sideLength)
+
+var optionalSquare: Square? = Square(sideLength: 2.5, name: "optional square")
+//optionalSquare = nil
+let sideLength = optionalSquare?.sideLength
+print(sideLength)
 
